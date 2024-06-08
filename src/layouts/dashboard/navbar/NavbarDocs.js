72ab4f604e130c947a113b3eb51f7e8faea5818b
Sahand Golkar <golkar.sahand@gmail.com>
2024-06-09 02:43:09 +0330

    ok

--- src/layouts/dashboard/navbar/NavbarDocs.js
+++ src/layouts/dashboard/navbar/NavbarDocs.js
@@ -1,11 +1,10 @@
 // @mui
-import { Stack, Button, Typography } from '@mui/material';
+import { Stack, Button, Typography } from "@mui/material";
 // hooks
-import useAuth from '../../../hooks/useAuth';
 // routes
-import { PATH_DOCS } from '../../../routes/paths';
+import { PATH_DOCS } from "../../../routes/paths";
 // assets
-import { DocIllustration } from '../../../assets';
+import { DocIllustration } from "../../../assets";
 
 // ----------------------------------------------------------------------
 
@@ -15,7 +14,14 @@ export default function NavbarDocs() {
   return (
     <Stack
       spacing={3}
-      sx={{ px: 5, pb: 5, mt: 10, width: 1, textAlign: 'center', display: 'block' }}
+      sx={{
+        px: 5,
+        pb: 5,
+        mt: 10,
+        width: 1,
+        textAlign: "center",
+        display: "block",
+      }}
     >
       <DocIllustration sx={{ width: 1 }} />
 
@@ -23,13 +29,18 @@ export default function NavbarDocs() {
         <Typography gutterBottom variant="subtitle1">
           Hi, {user?.displayName}
         </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
           Need help?
           <br /> Please check our docs
         </Typography>
       </div>
 
-      <Button href={PATH_DOCS} target="_blank" rel="noopener" variant="contained">
+      <Button
+        href={PATH_DOCS}
+        target="_blank"
+        rel="noopener"
+        variant="contained"
+      >
         Documentation
       </Button>
     </Stack>
